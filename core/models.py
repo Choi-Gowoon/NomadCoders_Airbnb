fb85c2a8c3dabf88ba9f9bc20c0e1a0be49c1b23
choigone <gowoonvv@gmail.com>
2020-02-18 17:09:46 +0900

    finish to make the model of rooms

---
+++ core/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+
+    """ Time Stamped Model """
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    # Set abstract class to do not update to database, just data model for extension
+    class Meta:
+        abstract = True
